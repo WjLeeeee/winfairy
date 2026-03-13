98c374b41cd284d9ac924b98d1fa8447285a14f1
woojin-tionnet <dldnwls0115@tionnet.com>
2026-03-13 14:44:51 +0900

    Feat - 모델 정의
    1. 경기결과, 구장이름, 가변 변수 모델 정의

---
+++ core/model/src/main/java/com/woojin/winfairy/core/model/GameResult.kt
@@ -0,0 +1,5 @@
+package com.woojin.winfairy.core.model
+
+enum class GameResult {
+    WIN, LOSE, DRAW, CANCELED
+}
---
+++ core/model/src/main/java/com/woojin/winfairy/core/model/VariableCategory.kt
@@ -0,0 +1,8 @@
+package com.woojin.winfairy.core.model
+
+enum class VariableCategory(val displayName: String) {
+    COMPANION("동반자"),
+    SEAT("좌석"),
+    UNIFORM("유니폼"),
+    FOOD("음식"),
+}
