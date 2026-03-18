0e8abd839d695fe554c82d163e9458c505ee5871
woojin-tionnet <dldnwls0115@tionnet.com>
2026-03-18 10:07:56 +0900

    Feat - 팀 로고 리소스 추가
    1. 팀 로고 리소스 추가
    2. 팀의 로고를 가져오는 함수 추가

---
+++ core/ui/src/main/java/com/woojin/winfairy/core/ui/TeamLogo.kt
@@ -0,0 +1,16 @@
+package com.woojin.winfairy.core.ui
+
+import com.woojin.winfairy.core.model.KboTeam
+
+fun KboTeam.logoRes(): Int = when (this) {
+    KboTeam.DooSan -> R.drawable.logo_doosan
+    KboTeam.Lg -> R.drawable.logo_lg
+    KboTeam.Samsung -> R.drawable.logo_samsung
+    KboTeam.Kiwoom -> R.drawable.logo_kiwoom
+    KboTeam.Kt -> R.drawable.logo_kt
+    KboTeam.Ssg -> R.drawable.logo_ssg
+    KboTeam.Kia -> R.drawable.logo_kia
+    KboTeam.HanHwa -> R.drawable.logo_hanwha
+    KboTeam.Nc -> R.drawable.logo_nc
+    KboTeam.Lotte -> R.drawable.logo_lotte
+}
