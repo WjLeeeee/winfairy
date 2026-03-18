4795ad4367399fa1953e926656385a829dc9684c
woojin-tionnet <dldnwls0115@tionnet.com>
2026-03-18 10:43:33 +0900

    Feat - 온보딩 화면 팀 선택시 선택된 팀의 색상을 테마의 primary컬러로 지정

--- app/src/main/java/com/woojin/winfairy/MainActivity.kt
+++ app/src/main/java/com/woojin/winfairy/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.woojin.winfairy.core.designsystem.theme.WinFairyTheme
+import com.woojin.winfairy.core.model.KboTeam
 import com.woojin.winfairy.core.navigation.AddRecord
 import com.woojin.winfairy.core.navigation.Home
 import com.woojin.winfairy.core.navigation.Onboarding
@@ -22,23 +27,26 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            WinFairyTheme {
+            var selectedTeam by remember { mutableStateOf<KboTeam?>(null) }
+
+            WinFairyTheme(team = selectedTeam) {
                 val navController = rememberNavController()
                 NavHost(
                     navController = navController,
                     startDestination = Onboarding
                 ) {
                     composable<Onboarding> {
-                        OnboardingScreen({
+                        OnboardingScreen { selectedKboTeam ->
+                            selectedTeam = selectedKboTeam
                             navController.navigate(Home) {
                                 popUpTo(Onboarding) { inclusive = true }
                             }
-                        })
+                        }
                     }  // feature:onboarding에서 가져옴
                     composable<Home> {
-                        HomeScreen({
+                        HomeScreen {
                             navController.navigate(AddRecord)
-                        })
+                        }
                     }              // feature:home에서 가져옴
                     composable<AddRecord> {
                         AddRecordScreen()
--- feature/home/src/main/java/com/woojin/winfairy/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/woojin/winfairy/feature/home/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +22,7 @@ fun HomeScreen(onComplete: () -> Unit) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Color.Magenta)
+                .background(MaterialTheme.colorScheme.primary)
                 .padding(innerPadding)
         ) {
             Text(
