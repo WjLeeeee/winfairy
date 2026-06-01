e35144cc1d1b45fa94a2cc32f35257ad662b41f4
woojin-tionnet <dldnwls0115@tionnet.com>
2026-06-01 14:03:23 +0900

    Chore - versionCode 4로 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.woojin.winfairy"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
